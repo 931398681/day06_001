0228d340755664e0828e44ea49930d891ba5244c
HZL <931398681@qq.com>
2020-02-14 13:11:39 +0800

    IHRM_员工管理

---
+++ run_suite.py
@@ -0,0 +1,9 @@
+# 导包
+import unittest
+from tools.HTMLTestRunner import HTMLTestRunner
+
+# 组装测试套件
+suite = unittest.defaultTestLoader.discover("./scripts", pattern="test*.py")
+# 获取报告存储文件流 实例化HTMLTestRunner 调用run执行测试套件
+with open("./report/report.html", "wb") as f:
+    HTMLTestRunner(stream=f).run(suite)
